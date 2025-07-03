ef2738db816013d5e6b18954920d1076a66e3d79
Jhalakupadhyay <jhalak92upadhyay@gmail.com>
2025-07-03 13:27:08 +0530

    fix: fixed the annotation issue.

--- src/main/java/com/a2y/salesHelper/pojo/Cooldown.java
+++ src/main/java/com/a2y/salesHelper/pojo/Cooldown.java
@@ -1,5 +1,7 @@
 package com.a2y.salesHelper.pojo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,10 +11,12 @@ import java.time.OffsetDateTime;
 
 @Data
 @Builder
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class Cooldown {
 
+    @Id
     private long orgId;
     private OffsetDateTime cooldownPeriod1;
     private OffsetDateTime cooldownPeriod2;
