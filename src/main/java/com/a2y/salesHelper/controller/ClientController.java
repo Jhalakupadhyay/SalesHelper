432462007f19e6887470f05e66a2a051c908af89
Jhalakupadhyay <jhalak92upadhyay@gmail.com>
2025-09-28 20:00:28 +0530

    feat: corrected the api call.

--- src/main/java/com/a2y/salesHelper/controller/ClientController.java
+++ src/main/java/com/a2y/salesHelper/controller/ClientController.java
@@ -6,13 +6,12 @@ import com.a2y.salesHelper.service.interfaces.CooldownService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
-@RestController( "api/client" )
+@RestController
+@RequestMapping("api/client")
 public class ClientController {
 
     private final CooldownService cooldownService;
